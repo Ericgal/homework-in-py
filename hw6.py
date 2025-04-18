0c51376c5f038eb9e4a6588ebd786b83d373a01e
Ericgal <yjjagal@gmail.com>
2025-04-18 21:31:30 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for j in range(1,10): 
+    for i in range(2,6):
+        p = j * i 
+        print(f'{i} x {j} = {p}', end='\t' )
+    print()
+print()
+for j in range(1,10): 
+    for i in range(6,10):
+        p = j * i 
+        print(f'{i} x {j} = {p}', end='\t' )
+    print()
